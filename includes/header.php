96af7a8b87b2170f8b1427a1d2ec4b307e6deefc
Github-Minghui <wangmih803@gmail.com>
2014-12-11 01:37:25 -0500

    modified sell_form.php

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -11,8 +11,8 @@
       <a class="navbar-brand" href="index.php">Libros</a>
     </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		<?php include 'includes/nav-menu.php';?>
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+		  <?php include 'includes/nav-menu.php';?>
   		<?php include 'includes/nav-left.php';?>
   		<?php include 'includes/nav-right.php';?>
 	</div><!-- /.navbar-collapse -->
--- sell_form.php
+++ sell_form.php
@@ -18,7 +18,6 @@ if (empty($_POST) ===false){
 		}
 	}
 }
-
 ?>
   <!-- Content of page -->
   <div class="col-md-9 col-sm-9 ">
@@ -53,44 +52,8 @@ if (empty($_POST) ===false){
 		  </li>
 	  </form>
     </div>
-    <div class= "panel panel-primary">
-      <div class="panel-heading">
-       <h3>Books Iventory: (<-TODO)</h3>
-      </div>
-	  <li class="list-group-item">
-	  <div class="row">
-			<div class="col-md-3"> <!--this is the size of the box of the image--> 
-				<a href="bookpage.php" class="thumbnail">
-					<img src="image/rec_book1.png"  height = "150px" width = "150px"> <!--get image and size-->
-						<span>J.K. Rowling</span><br><!--this is meant to type the author's name--> 
-					<span class="text-muted">Fantasy Fiction</span> <!-- this is for genre etc.--> 
-				</a>
-			</div>
-			<div class="col-md-3"> <!--this is the size of the box of the image--> 
-				<a href="bookpage.php" class="thumbnail">
-					<img src="image/rec_book2.png"  height = "150px" width = "150px"> <!--get image and size-->
-						<span>J.K. Rowling</span><br><!--this is meant to type the author's name--> 
-					<span class="text-muted">Fantasy Fiction</span> <!-- this is for genre etc.--> 
-				</a>
-			</div>
-			<div class="col-md-3"> <!--this is the size of the box of the image--> 
-				<a href="bookpage.php" class="thumbnail">
-					<img src="image/rec_book3.png"  height = "150px" width = "150px"> <!--get image and size-->
-						<span>J.K. Rowling</span><br><!--this is meant to type the author's name--> 
-					<span class="text-muted">Fantasy Fiction</span> <!-- this is for genre etc.--> 
-				</a>
-			</div>
-			<div class="col-md-3"> <!--this is the size of the box of the image--> 
-				<a href="bookpage.php" class="thumbnail">
-					<img src="image/rec_book4.png"  height = "150px" width = "150px"> <!--get image and size-->
-						<span>J.K. Rowling</span><br><!--this is meant to type the author's name--> 
-					<span class="text-muted">Fantasy Fiction</span> <!-- this is for genre etc.--> 
-				</a>
-			</div>
-	  </div>
-	  </li>                       
-    </div>
-  </div>  
+	<?php include 'includes/widgets/recommendation.php';?>                       
+  </div>
   <div class="col-md-3 col-xs-12">
   	<?php include 'includes/widgets/side_recommendation.php';?>
   </div> 
