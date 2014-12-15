5340eb93cab077db09d3d4de7498e59402cc5604
enmana21@gmail.com <enmana21@gmail.com>
2014-12-14 19:01:47 -0500

    added M's citymail to the project

--- includes/widgets/help.php
+++ includes/widgets/help.php
@@ -6,8 +6,10 @@
 
 <li class="list-group-item">	  
 	Email: <a href="mailto:ealmanz04@citymail.cuny.edu">ealmanz04@citymail.cuny.edu</a> 
-	Email: <a href="mailto:mwang13@citymail.cuny.edu">mwang13@citymail.cuny.edu</a> 
-	Email: <a href="mailto:muhthadunrahman@gmail.com">muhthadunrahman@gmail.com</a> 
+	<br>
+	Email: <a href="mailto:mwang13@citymail.cuny.edu">mwang13@citymail.cuny.edu</a>
+	<br>
+	Email: <a href="mailto:mrahman44@citymail.cuny.edu">mrahman44@citymail.cuny.edu</a> 
 	<br>
 	Phone: xxx-xxx-xxxx (Email is best option at the moment)
 </li>
